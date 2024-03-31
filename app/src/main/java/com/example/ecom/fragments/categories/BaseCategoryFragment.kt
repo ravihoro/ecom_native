3471f22b19bd30636868016697ca27d102a8beed
ravihoro <ravijohnhoro5@gmail.com>
2024-03-31 15:05:49 +0530

    added recycler view for best deals and category

--- app/src/main/java/com/example/ecom/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/ecom/fragments/categories/BaseCategoryFragment.kt
@@ -1,7 +1,56 @@
 package com.example.ecom.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ecom.R
+import com.example.ecom.adapters.BestProductsAdapter
+import com.example.ecom.databinding.FragmentBaseCategoryBinding
 
 open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
+
+    private lateinit var binding: FragmentBaseCategoryBinding
+
+    private lateinit var offerAdapter: BestProductsAdapter
+    private lateinit var bestProductsAdapter: BestProductsAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRv()
+        setupBestProductsRv()
+
+
+    }
+
+    private fun setupOfferRv() {
+        offerAdapter = BestProductsAdapter()
+        binding.rvOffer.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+        }
+
+    }
+
+    private fun setupBestProductsRv() {
+        bestProductsAdapter = BestProductsAdapter()
+        binding.rvBestDeals.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+        }
+    }
+
 }
